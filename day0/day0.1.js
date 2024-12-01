f204fb94cd3bd0e524570956105c8d2cd26d01c7
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-12-01 21:32:14 +0200

    chore: repo init

---
+++ day0/day0.1.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, 'day.input'), 'utf8', (err, data) => {
+    data.split("\n").forEach((line, index) => {
+    });
+});
