71911888a9bc08a1b937a59bb62335461d5d62ea
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-12-07 11:18:28 +0200

    feat: day 7

--- day7/day7.1.js
+++ day7/day7.1.js
@@ -1,7 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day7.input.ex'), 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, 'day7.input'), 'utf8', (err, data) => {
+    let sum = 0;
+
     data.split("\n").forEach((line, index) => {
+        let [ target, list] = line.split(': ');
+
+        target = parseInt(target, 10);
+        list = list.split(' ').map(element => parseInt(element, 10));
+        const maxMask = 2 ** (list.length - 1);
+
+        for (let i = 0; i < maxMask; i++) {
+            if (sultiply(list, i, target) === target) {
+                sum += target;
+
+                return;
+            }
+        }
     });
+
+    console.log(sum);
 });
+
+function sultiply(list, mask, limit) {
+    let result = (mask & 1) === 0 ? list[0] + list[1] : list[0] * list[1];
+
+    for (let i = 1; i < list.length - 1; i++) {
+        const bit = (mask >> i) & 1;
+
+        result = bit === 0 ? result + list[i + 1] : result * list[i + 1];
+
+        if (result > limit) {
+            return -1;
+        }
+    }
+
+    return result;
+}
--- day7/day7.2.js
+++ day7/day7.2.js
@@ -1,7 +1,50 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day7.input.ex'), 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, 'day7.input'), 'utf8', (err, data) => {
+    let sum = 0;
+
     data.split("\n").forEach((line, index) => {
+        let [ target, list] = line.split(': ');
+
+        target = parseInt(target, 10);
+        list = list.split(' ').map(element => parseInt(element, 10));
+        const maxMask = 3 ** (list.length - 1);
+
+        for (let i = 0; i < maxMask; i++) {
+            if (sultipate(list, i, target) === target) {
+                sum += target;
+
+                return;
+            }
+        }
     });
+
+    console.log(sum);
 });
+
+// yes, sum & multiply & concatenate...
+function sultipate(list, mask, limit) {
+    const operation = mask % 3;
+    let result;
+    if (operation === 0) {
+        result = list[0] + list[1];
+    } else if (operation === 1) {
+        result = list[0] * list[1];
+    } else {
+        result = parseInt(`${list[0]}${list[1]}`);
+    }
+
+    for (let i = 1; i < list.length - 1; i++) {
+        const operation = Math.floor(mask / Math.pow(3, i)) % 3;
+        if (operation === 0) {
+            result += list[i + 1];
+        } else if (operation === 1) {
+            result *= list[i + 1];
+        } else {
+            result = parseInt(`${result}${list[i + 1]}`);
+        }
+    }
+
+    return result;
+}
