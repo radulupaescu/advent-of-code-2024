c5cfd45191daee4c8b9d6d97442ca08592b24c26
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-12-09 10:19:36 +0200

    feat: day 9

--- day9/day9.1.js
+++ day9/day9.1.js
@@ -1,7 +1,48 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day9.input.ex'), 'utf8', (err, data) => {
-    data.split("\n").forEach((line, index) => {
-    });
+fs.readFile(path.join(__dirname, 'day9.input'), 'utf8', (err, data) => {
+    const disk = data.split('').map(Number);
+    let lastFileId = Math.floor(disk.length / 2);
+    let lastIndex = disk.length % 2 === 1 ? disk.length - 1 : disk.length - 2;
+    let crrIndex = 0;
+    let checksum = 0;
+
+    let start = 0;
+    let fileId = 0;
+    while (crrIndex <= lastIndex) {
+        let blockLen = disk[crrIndex];
+        if (crrIndex % 2 === 0) { // crr index on file
+            checksum += blockFileSum(start, start + blockLen - 1,  fileId);
+            fileId++;
+            start += blockLen;
+        } else { // crr index on space
+            let remainingBlockLen = blockLen;
+            do {
+                if (remainingBlockLen >= disk[lastIndex]) {
+                    checksum += blockFileSum(start, start + disk[lastIndex] - 1, lastFileId);
+
+                    remainingBlockLen -= disk[lastIndex];
+                    start += disk[lastIndex];
+
+                    lastFileId -= 1;
+                    lastIndex -= 2;
+                } else {
+                    checksum += blockFileSum(start, start + remainingBlockLen - 1, lastFileId);
+                    start += remainingBlockLen;
+
+                    disk[lastIndex] -= remainingBlockLen;
+                    remainingBlockLen = 0;
+                }
+            } while (remainingBlockLen > 0)
+        }
+
+        crrIndex++;
+    }
+
+    console.log(checksum);
 });
+
+function blockFileSum(startIndex, stopIndex, fileId) {
+    return fileId * 0.5 * ((stopIndex * (stopIndex + 1)) - startIndex * (startIndex - 1));
+}
--- day9/day9.2.js
+++ day9/day9.2.js
@@ -1,7 +1,58 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day9.input.ex'), 'utf8', (err, data) => {
-    data.split("\n").forEach((line, index) => {
-    });
+fs.readFile(path.join(__dirname, 'day9.input'), 'utf8', (err, data) => {
+    const disk = data.split('').map(Number);
+    let checksum = 0;
+    let fileId = 0;
+
+    const fs = [];
+    for (let i = 0; i < disk.length; i++) {
+        if (i % 2 === 0) {
+            fs.push({ id: fileId, size: disk[i] });
+            fileId++;
+        } else {
+            fs.push({ id: -1, size: disk[i] });
+        }
+    }
+
+    const defragged = [];
+    for (let i = 0; i < fs.length; i++) {
+        if (fs[i].id === -1) {
+            let element = fs.length - 1;
+
+            while (fs[i].size > 0 && element > i) {
+                if (fs[element].id !== -1 && fs[element].size <= fs[i].size) {
+                    defragged.push({ ...fs[element] });
+                    fs[i].size -= fs[element].size;
+                    fs[element].id = -1;
+                }
+
+                element--;
+            }
+
+            if (fs[i].size > 0) {
+                defragged.push(fs[i]);
+            }
+        } else if (fs[i].size !== 0) {
+            defragged.push(fs[i]);
+        }
+    }
+
+    let start = 0;
+    while (defragged.length > 0) {
+        let element = defragged.shift();
+        if (element.id !== -1) {
+            checksum += blockFileSum(start, start + element.size - 1, element.id);
+        }
+
+        start += element.size;
+    }
+
+    console.log(checksum);
 });
+
+function blockFileSum(startIndex, stopIndex, fileId) {
+    return fileId * 0.5 * ((stopIndex * (stopIndex + 1)) - startIndex * (startIndex - 1));
+}
+
