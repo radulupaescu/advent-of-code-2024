1396f7b12503ec383e120de1ce755fc4d48fca18
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-12-13 08:59:06 +0200

    feat: day 13

--- day13/day13.1.js
+++ day13/day13.1.js
@@ -1,7 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day13.input.ex'), 'utf8', (err, data) => {
-    data.split("\n").forEach((line, index) => {
+fs.readFile(path.join(__dirname, 'day13.input'), 'utf8', (err, data) => {
+    let tokens = 0;
+
+    data.split("\n\n").forEach((group, index) => {
+        const lines = group.split("\n");
+
+        const buttonA = lines[0].split(': ')[1].split(', ').map((v) => v.split('+')[1]).map(Number);
+        const buttonB = lines[1].split(': ')[1].split(', ').map((v) => v.split('+')[1]).map(Number);
+        const prize = lines[2].split(': ')[1].split(', ').map((v) => v.split('=')[1]).map(Number);
+
+        tokens += strategy(buttonA, buttonB, prize);
     });
+
+    console.log(tokens);
 });
+
+function strategy(buttonA, buttonB, prize) {
+    const combinations = [];
+
+    for (let aPushes = 1; aPushes <= 100; aPushes++) {
+        const bBushes = (prize[0] - aPushes * buttonA[0]) / buttonB[0];
+        if (Math.floor(bBushes) === bBushes
+            && (bBushes * buttonB[1]) + (aPushes * buttonA[1]) === prize[1]
+        ) {
+            combinations.push([aPushes, bBushes]);
+        }
+    }
+
+    if (combinations.length === 0) {
+        return 0;
+    }
+
+    let minTokens = 9999999999999;
+    combinations.forEach((pair) => {
+        const cost = pair[0] * 3 + pair[1];
+        if (cost < minTokens) {
+            minTokens = cost;
+        }
+    });
+
+    return minTokens;
+}
--- day13/day13.2.js
+++ day13/day13.2.js
@@ -1,7 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day13.input.ex'), 'utf8', (err, data) => {
-    data.split("\n").forEach((line, index) => {
+fs.readFile(path.join(__dirname, 'day13.input'), 'utf8', (err, data) => {
+    let tokens = 0;
+
+    data.split("\n\n").forEach((group, index) => {
+        const lines = group.split("\n");
+
+        const buttonA = lines[0].split(': ')[1].split(', ').map((v) => v.split('+')[1]).map(Number);
+        const buttonB = lines[1].split(': ')[1].split(', ').map((v) => v.split('+')[1]).map(Number);
+        const prize = lines[2].split(': ')[1].split(', ').map((v) => v.split('=')[1]).map(Number);
+
+        tokens += strategy(buttonA, buttonB, prize.map(c => c + 10000000000000));
     });
+
+    console.log(tokens);
 });
+
+// Cramer's rule https://en.wikipedia.org/wiki/Cramer%27s_rule
+function strategy(buttonA, buttonB, prize) {
+    const delta = buttonA[0] * buttonB[1] - buttonB[0] * buttonA[1];
+    const deltaX = prize[0] * buttonB[1] - prize[1] * buttonB[0];
+    const deltaY = prize[1] * buttonA[0] - prize[0] * buttonA[1];
+
+    if (delta !== 0) {
+        const aPushes = deltaX / delta;
+        const bPushes = deltaY / delta;
+
+        if (Math.floor(aPushes) === aPushes && Math.floor(bPushes) === bPushes) {
+            return aPushes * 3 + bPushes;
+        }
+    }
+
+    return 0;
+}
