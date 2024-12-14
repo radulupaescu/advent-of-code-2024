345ad679712d7c8d00e034bbd7b342844aea6449
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-12-14 11:08:11 +0200

    feat: day 14

--- day14/day14.1.js
+++ day14/day14.1.js
@@ -1,7 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day14.input.ex'), 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, 'day14.input'), 'utf8', (err, data) => {
+    const positions = [];
+    const X = 101;
+    const Y = 103;
+
+    const midX = Math.floor(X / 2);
+    const midY = Math.floor(Y / 2);
+    const seconds = 100;
+
     data.split("\n").forEach((line, index) => {
+        const [p, v] = line.split(' ')
+            .map((e) => e.split('=')[1])
+            .map((e) => e.split(',').map(Number))
+        ;
+
+        let x = (p[0] + seconds * v[0]) % X;
+        let y = (p[1] + seconds * v[1]) % Y;
+
+        x = x < 0 ? X + x : x;
+        y = y < 0 ? Y + y : y;
+
+        positions.push([x, y]);
     });
+
+    const quadrants = [0, 0, 0, 0];
+    positions.forEach((pos) => {
+        if (pos[0] < midX && pos[1] < midY) { quadrants[0]++; }
+        if (pos[0] > midX && pos[1] < midY) { quadrants[1]++; }
+        if (pos[0] < midX && pos[1] > midY) { quadrants[2]++; }
+        if (pos[0] > midX && pos[1] > midY) { quadrants[3]++; }
+    });
+
+    console.log(quadrants.reduce((prod, v) => prod * v, 1));
 });
--- day14/day14.2.js
+++ day14/day14.2.js
@@ -1,7 +1,78 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-fs.readFile(path.join(__dirname, 'day14.input.ex'), 'utf8', (err, data) => {
+const X = 101;
+const Y = 103;
+const bots = [];
+let currentFrame = 0;
+
+fs.readFile(path.join(__dirname, 'day14.input'), 'utf8', (err, data) => {
     data.split("\n").forEach((line, index) => {
+        const [p, v] = line.split(' ')
+            .map((e) => e.split('=')[1])
+            .map((e) => e.split(',').map(Number))
+        ;
+
+        bots.push([p, v]);
     });
 });
+
+function posAtSecond(position, velocity, second) {
+    let x = (position[0] + second * velocity[0]) % X;
+    let y = (position[1] + second * velocity[1]) % Y;
+
+    x = x < 0 ? X + x : x;
+    y = y < 0 ? Y + y : y;
+
+    return [x, y];
+}
+
+function initializeMap() {
+    const map = [];
+    for (let i = 0; i < Y; i++) {
+        map.push((new Array(X)).fill('.'));
+    }
+
+    return map;
+}
+
+function placeRobots(positions, map) {
+    positions.forEach((pos) => {
+        map[pos[1]][pos[0]] = '#';
+    });
+
+    return map;
+}
+
+function printBoard(map) {
+    const toPrint = [];
+    for (let i = 0; i < map.length; i++) {
+        toPrint.push(map[i].join(''));
+    }
+    console.log(toPrint.join('\n'));
+}
+
+function renderFrame(second) {
+    console.clear();
+    console.log(`Frame: ${second}`);
+
+    let positions = bots.map((bot) => posAtSecond(bot[0], bot[1], second));
+    printBoard(placeRobots(positions, initializeMap()));
+}
+
+function handleKeyPress(str, key) {
+    if (key.name === 'right') {
+        currentFrame += 1;
+    } else if (key.name === 'left') {
+        currentFrame = currentFrame - 1 < 0 ? 0 : currentFrame - 1;
+    } else if (key.name === 'q') {
+        process.exit();
+    }
+
+    renderFrame(currentFrame);
+}
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', handleKeyPress);
