e8a43fd3d1748bf391a6974e2ca6cf2f0df7395a
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-12-17 13:26:00 +0200

    chore: day 18 setup

---
+++ day18/day18.2.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, 'day18.input.ex'), 'utf8', (err, data) => {
+    data.split("\n").forEach((line, index) => {
+    });
+});
