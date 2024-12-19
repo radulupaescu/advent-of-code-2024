74799115533528d52a9a7c949e94955b01cdac70
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-12-19 09:06:13 +0200

    feat: day 19

--- day19/day19.1.js
+++ day19/day19.1.js
@@ -1,7 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day19.input.ex'), 'utf8', (err, data) => {
-    data.split("\n").forEach((line, index) => {
+fs.readFile(path.join(__dirname, 'day19.input'), 'utf8', (err, data) => {
+    const [designs, patterns] = data.split("\n\n").map((raw, index) => {
+        if (index === 0) {
+            return raw.split(', ');
+        } else {
+            return raw.split("\n");
+        }
     });
+
+    let count = 0;
+    patterns.forEach((pattern) => {
+        if (canArrange(designs, pattern)) {
+            count++;
+        }
+    });
+
+    console.log(count);
 });
+
+function canArrange(designs, pattern) {
+    const n = pattern.length;
+    const dp = Array(n + 1).fill(false);
+    dp[0] = true;
+
+    for (let i = 1; i <= n; i++) {
+        for (const design of designs) {
+            if (i >= design.length && pattern.slice(i - design.length, i) === design) {
+                dp[i] = dp[i] || dp[i - design.length];
+            }
+        }
+    }
+
+    return dp[n];
+}
--- day19/day19.2.js
+++ day19/day19.2.js
@@ -1,7 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'day19.input.ex'), 'utf8', (err, data) => {
-    data.split("\n").forEach((line, index) => {
+fs.readFile(path.join(__dirname, 'day19.input'), 'utf8', (err, data) => {
+    const [designs, patterns] = data.split("\n\n").map((raw, index) => {
+        if (index === 0) {
+            return raw.split(', ');
+        } else {
+            return raw.split("\n");
+        }
     });
+
+    let count = 0;
+    patterns.forEach((pattern) => {
+        count += countArrangements(designs, pattern);
+    });
+
+    console.log(count);
 });
+
+function countArrangements(designs, pattern) {
+    const n = pattern.length;
+    const dp = Array(n + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= n; i++) {
+        for (const design of designs) {
+            if (i >= design.length && pattern.slice(i - design.length, i) === design) {
+                dp[i] += dp[i - design.length];
+            }
+        }
+    }
+
+    return dp[n];
+}
